c29deb54ce28a407dc46a76ff6bef5a95e10d40b
Rorisang Putu <rorisangputu@gmail.com>
2024-08-11 21:27:01 +0200

    styling clients

--- src/Components/Consult.jsx
+++ src/Components/Consult.jsx
@@ -4,9 +4,9 @@ const Consult = () => {
     return (
         <div className="w-full py-5">
             <div className="w-[90%] container mx-auto h-[20vh] flex items-center justify-center">
-                <div className="flex flex-col md:flex-row items-center gap-20  ">
-                    <div className="">
-                        <h1 className="text-3xl md:text-5xl my-4">Start optimizing today!</h1>
+                <div className="flex flex-col md:flex-row items-center md:gap-20 gap-5  ">
+                    <div className="flex flex-col justify-center items-center text-center md:items-start">
+                        <h1 className="text-3xl md:text-5xl my-4">Start optimizing today.</h1>
                         <p className="">Start delegating tasks and reclain your valuable time.
                             Let&apos;s revolutionize the way you work together!
                         </p>
